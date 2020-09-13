1c2814d107a70f6c51b7d3a4d8eb4de0d7257006
Sam Liu <sam@smscentral.com.au>
2020-09-14 00:12:46 +1000

    added some unit tests

--- tests/SimpleHttpClientTest.php
+++ tests/SimpleHttpClientTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use function PHPUnit\Framework\assertEquals;
 use PHPUnit\Framework\TestCase;
 use App\SimpleHttpClient\SimpleHttpClient;
 
@@ -16,7 +17,42 @@ class SimpleHttpClientTest extends TestCase
     public function testConstructor(): void
     {
         $url = 'http://example.com';
+        $header = [
+            'Content-type' => 'application/json'
+        ];
         $client = new SimpleHttpClient($url);
         self::assertEquals($url, $client->getUrl());
+        self::assertEquals($header, $client->getHeader());
+    }
+
+    public function testGetSetHeader(): void
+    {
+        $header = [
+          'Content-type' => 'text/xml'
+        ];
+        $client = new SimpleHttpClient('');
+        $client->setHeader($header);
+        self::assertEquals($header, $client->getHeader());
+        $newHeader = [
+            'Accept' => 'application/xml'
+        ];
+        $client->setHeader($newHeader);
+        self::assertEquals(
+            array_merge($header, $newHeader),
+            $client->getHeader()
+        );
+    }
+
+    public function testFormatHeader(): void
+    {
+        $header = [
+            'Content-type' => 'text/xml',
+            'Accept' => 'application/xml'
+        ];
+
+        $expected = "Content-type: text/xml\r\nAccept: application/xml\r\n";
+        $client = new SimpleHttpClient('');
+        $client->setHeader($header);
+        self::assertEquals($expected, $client->formatHeader());
     }
 }
